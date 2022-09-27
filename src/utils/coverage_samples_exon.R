7bcac20285c32e141f2bb9ee9aa312065d202390
Jan Svatoň <450402@mail.muni.cz>
2022-09-27 10:24:22 +0200

    Updates

--- src/utils/coverage_samples_exon.R
+++ src/utils/coverage_samples_exon.R
@@ -1,7 +1,7 @@
 library(data.table)
 args = commandArgs(trailingOnly=TRUE)
 path<-args[1]
-path<-"Z:/home/450402/000000-My_Documents/BTK_k8s_testing/launch/220708_TP53_20220708/coverage"
+#path<-"/storage/home/450402/000000-My_Documents/QC_k8s_testing/launch/000000-My_Documents/coverage"
 
 final_list<-list()
 samples_exons = list()
@@ -9,8 +9,10 @@ for(i in list.files(path, pattern = "*PBcoverage.txt")){
   
   # table of per base coverage for each sample (to paste together to one table)
   file <- read.table(paste0(path,"/",i), sep="\t")
-  colnames(file) <- c("Chrom", "Exon", "K_NICEMU", "Start", "End", "K_NICEMU2", "STRAND", "K_NICEMU3", "Base_number", "Coverage")
-  file <- file[,-c(3,6,8)]
+  #colnames(file) <- c("Chrom", "Exon", "K_NICEMU", "Start", "End", "K_NICEMU2", "STRAND", "K_NICEMU3", "Base_number", "Coverage")
+  colnames(file) <- c("Chrom", "Start", "End", "Exon", "Base_number", "Coverage")
+  
+  #file <- file[,-c(3,6,8)]
   file$sample <- gsub(".PBcoverage.txt", "", i)
   final_list[[i]] <- file
   
@@ -34,8 +36,8 @@ for(i in list.files(path, pattern = "*PBcoverage.txt")){
   
 }
 
-write.table(samples_exons, file=paste0(path,"/",args[2], ".perexon_samples.txt"),
+write.table(samples_exons, file=paste0(args[2], ".perexon_samples.txt"),
             sep="\t", quote = F, row.names = F)
 final_df<-rbindlist(final_list)
-write.table(final_df, file=paste0(path,"/",args[2], "_tp53_gene_coverage.txt"),
+write.table(final_df, file=paste0(path,"/",args[2], "_AteroHemo_gene_coverage.txt"),
             sep="\t", quote = F, row.names = F)
